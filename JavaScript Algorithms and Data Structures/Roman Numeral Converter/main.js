4c6dc05ffba1c5fd2345bbc1cd178fc97b65589d
JNCJcoder <jorgenettocjunior@gmail.com>
2022-05-16 11:34:26 -0300

    JavaScript Algorithms and Data Structures - Roman Numeral Converter concluido

---
+++ JavaScript Algorithms and Data Structures/Roman Numeral Converter/main.js
@@ -0,0 +1,31 @@
+function convertToRoman(num)
+{
+    const romanValues = {
+        M:  1000,
+        CM: 900,
+        D:  500,
+        CD: 400,
+        C:  100,
+        XC: 90,
+        L:  50,
+        XL: 40,
+        X:  10,
+        IX: 9,
+        V:  5,
+        IV: 4,
+        I:  1,
+    };  
+
+    let result = "";
+
+    for(const value in romanValues)
+    {
+        result += value.repeat(Math.floor(num / romanValues[value]));
+        num %= romanValues[value];
+    }
+
+
+    return result;
+}
+
+convertToRoman(36);
