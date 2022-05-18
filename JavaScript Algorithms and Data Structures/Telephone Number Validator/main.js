e8c40361f7422b4aed9241003e751b4a64d2b930
JNCJcoder <jorgenettocjunior@gmail.com>
2022-05-18 11:14:22 -0300

    JavaScript Algorithms and Data Structures - Telephone Number Validator concluido

---
+++ JavaScript Algorithms and Data Structures/Telephone Number Validator/main.js
@@ -0,0 +1,8 @@
+function telephoneCheck(str)
+{
+    const regex = /1{0,1}\s{0,1}((\(\d{3}\))|(\d{3}))[-]{0,1}[ ]{0,1}\d{3}[- ]{0,1}\d{4}/;
+
+    return regex.test(str) && str.match(regex)[0] === str;
+}
+
+telephoneCheck("555-555-5555");
